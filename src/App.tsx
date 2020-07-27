d904fb62de6c76a1eb8db5d833e6c62063b1ea43
Julio <july2m3@gmail.com>
2020-07-27 18:52:53 -0500

    Added snake game path to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import MemoryGame from './memory-game/Main';
+import SnakeGame from './snake-game';
+
 import './App.css';
 
 const Home = () => <h1>Welcome Home</h1>;
@@ -12,26 +14,32 @@ const App = () => (
       <nav>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to='/memory-game'>Cat Memory Game</Link>
+            <Link to="/memory-game">Cat Memory Game</Link>
+          </li>
+          <li>
+            <Link to="/snake-game">Snake Game</Link>
           </li>
         </ul>
       </nav>
 
       <main>
         <Switch>
-          <Route path='/about'>
+          <Route path="/about">
             <About />
           </Route>
-          <Route path='/memory-game'>
+          <Route path="/memory-game">
             <MemoryGame />
           </Route>
-          <Route path='/'>
+          <Route path="/snake-game">
+            <SnakeGame />
+          </Route>
+          <Route path="/">
             <Home />
           </Route>
         </Switch>
