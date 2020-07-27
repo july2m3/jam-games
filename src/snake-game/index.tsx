5377cc7ac184dae6077dbeaa6eb73fb88c238ae2
Julio <july2m3@gmail.com>
2020-07-27 18:51:07 -0500

    Added snake game index file

---
+++ src/snake-game/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IProps {}
+
+const SnakeGame = (props: IProps) => {
+  return (
+    <div className="snake-game">
+      <h1>Welcome to my snake game</h1>
+    </div>
+  );
+};
+
+export default SnakeGame;
